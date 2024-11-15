2c820ebd9ca09ad8ae71a46de1b07b2ec666e726
arismetyogi <arismetyogi@gmail.com>
2024-11-15 15:59:50 +0700

    publish livewire config and add title variable to users page full page component

--- app/Livewire/UsersPage.php
+++ app/Livewire/UsersPage.php
@@ -3,9 +3,11 @@
 namespace App\Livewire;
 
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Layout('layouts.app')]
+#[Title('Users')]
 class UsersPage extends Component
 {
     public function render()
