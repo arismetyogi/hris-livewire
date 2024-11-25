7323246b37948eec96b98356b19fa25a7f260883
arismetyogi <arismetyogi@gmail.com>
2024-11-25 12:47:05 +0700

    added department model and migration

--- app/Livewire/Forms/UserForm.php
+++ app/Livewire/Forms/UserForm.php
@@ -4,22 +4,24 @@ namespace App\Livewire\Forms;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Livewire\Attributes\Rule;
+use Livewire\Attributes\Validate;
 use Livewire\Form;
 
 class UserForm extends Form
 {
     public ?User $user;
 
-    #[Rule('required|unique:users|min:6', as: 'Username')]
+    #[Validate('required|unique:users|min:6', as: 'Username')]
     public $username;
-    #[Rule('required|min:6', as: 'First Name')]
+    #[Validate('required|min:6', as: 'First Name')]
     public $first_name;
-    #[Rule(as: 'Last Name')]
+    #[Validate(as: 'Last Name')]
     public $last_name;
-    #[Rule('required|email|unique:users', as: 'Email')]
+    #[Validate('required|email|unique:users', as: 'Email')]
     public $email;
-    #[Rule('required|min:8', as: 'Password')]
+    #[Validate('required', as: 'Department')]
+    public $department_id;
+    #[Validate('required|min:8', as: 'Password')]
     public $password;
 
     public function setUser(User $user)
@@ -30,8 +32,10 @@ class UserForm extends Form
         $this->last_name = $user->last_name;
         $this->username = $user->username;
         $this->email = $user->email;
+        $this->department_id = $user->department_id;
         $this->password = Hash::make($user->password);
     }
+
     public function store()
     {
         User::create($this->except(['user']));
--- resources/views/livewire/user/create.blade.php
+++ resources/views/livewire/user/create.blade.php
@@ -9,48 +9,57 @@
             <div class="grid grid-cols-12 gap-4 sm:grid-cols-8">
                 <!-- Name -->
                 <div class="col-span-6 sm:col-span-4">
-                    <x-label for="form.first_name" value="{{ __('First Name') }}" />
+                    <x-label for="form.first_name" value="{{ __('First Name') }}"/>
                     <x-input id="form.first_name" type="text" class="block w-full mt-1" wire:model="form.first_name"
-                        required autocomplete="user.first_name" />
-                    <x-input-error for="form.first_name" class="mt-2" />
+                             required autocomplete="user.first_name"/>
+                    <x-input-error for="form.first_name" class="mt-2"/>
                 </div>
 
                 <div class="col-span-6 sm:col-span-4">
-                    <x-label for="form.last_name" value="{{ __('Last Name') }}" />
+                    <x-label for="form.last_name" value="{{ __('Last Name') }}"/>
                     <x-input id="form.last_name" type="text" class="block w-full mt-1" wire:model="form.last_name"
-                        autocomplete="user.last_name" />
-                    <x-input-error for="form.last_name" class="mt-2" />
+                             autocomplete="user.last_name"/>
+                    <x-input-error for="form.last_name" class="mt-2"/>
                 </div>
 
                 <!-- userName -->
                 <div class="col-span-6 sm:col-span-4">
-                    <x-label for="form.username" value="{{ __('Username') }}" />
+                    <x-label for="form.username" value="{{ __('Username') }}"/>
                     <x-input id="form.username" type="text" class="block w-full mt-1" wire:model="form.username"
-                        required autocomplete="user.username" />
-                    <x-input-error for="form.username" class="mt-2" />
+                             required autocomplete="user.username"/>
+                    <x-input-error for="form.username" class="mt-2"/>
                 </div>
 
                 <!-- Email -->
                 <div class="col-span-6 sm:col-span-4">
-                    <x-label for="form.email" value="{{ __('Email') }}" />
+                    <x-label for="form.email" value="{{ __('Email') }}"/>
                     <x-input id="form.email" type="email" class="block w-full mt-1" wire:model="form.email" required
-                        autocomplete="user.email" />
-                    <x-input-error for="form.email" class="mt-2" />
+                             autocomplete="user.email"/>
+                    <x-input-error for="form.email" class="mt-2"/>
+                </div>
+                <!-- Department -->
+                <div class="col-span-full">
+                    <x-label for="form.department" value="{{ __('Department') }}"/>
+                    <x-input id="form.department" type="text" class="block w-full mt-1"
+                             wire:model="form.department"
+                             required
+                             autocomplete="user.department"/>
+                    <x-input-error for="form.department" class="mt-2"/>
                 </div>
 
                 <!-- Password -->
                 <div class="col-span-6 sm:col-span-4">
-                    <x-label for="form.password" value="{{ __('Password') }}" />
+                    <x-label for="form.password" value="{{ __('Password') }}"/>
                     <x-input id="form.password" type="password" class="block w-full mt-1" wire:model="form.password"
-                        required />
-                    <x-input-error for="form.password" class="mt-2" />
+                             required/>
+                    <x-input-error for="form.password" class="mt-2"/>
                 </div>
 
                 <!-- Password Confirmation -->
                 <div class="col-span-6 sm:col-span-4">
-                    <x-label for="form.password_confirmation" value="{{ __('Password Confirmation') }}" />
-                    <x-input id="form.password_confirmation" type="password" class="block w-full mt-1" required />
-                    <x-input-error for="form.password_confirmation" class="mt-2" />
+                    <x-label for="form.password_confirmation" value="{{ __('Password Confirmation') }}"/>
+                    <x-input id="form.password_confirmation" type="password" class="block w-full mt-1" required/>
+                    <x-input-error for="form.password_confirmation" class="mt-2"/>
                 </div>
             </div>
         </x-slot>
